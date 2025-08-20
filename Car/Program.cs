8eca5a8ff42484c687ce147d618f9f8c65fface6
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-20 17:29:55 -0300

    Adds Car and Person

---
+++ Car/Program.cs
@@ -0,0 +1,56 @@
+﻿public abstract class BaseCar
+{
+    public int Speed { get; set; }
+    public string Name { get; set; }
+    public int Wheels = 4;
+
+
+    public BaseCar(int speed, string name)
+    {
+        Speed = speed;
+        Name = name;
+    }
+
+    public abstract string Specs();
+
+}
+
+public class Supercar : BaseCar
+{
+    public Supercar(int speed, string name) : base(speed, name) { }
+    public override string Specs()
+
+    {
+        return $"{Name} is speeding at {Speed}km/h.";
+
+    }
+
+
+}
+
+public class SportsCar : BaseCar
+{
+    public SportsCar(int speed, string name) : base(speed, name) { }
+    public override string Specs()
+    {
+        return $"{Name} is speeding at {Speed}km/h.";
+    }
+}
+
+
+public class Program
+{
+    static void Main()
+    {
+        var ferrari = new Supercar(450, "Ferrari Pininfarina");
+
+        Console.WriteLine(ferrari.Specs());
+        Console.WriteLine($"{ferrari.Name} has {ferrari.Wheels} wheels");
+
+        var civic = new SportsCar(293, "Honda Civic Type R");
+        Console.WriteLine(civic.Specs());
+        Console.WriteLine($"{civic.Name} has {civic.Wheels} wheels");
+
+    }
+
+}
---
+++ Person/Program.cs
@@ -0,0 +1,42 @@
+﻿class Person
+{
+
+    private string Reverser(string sentence)
+    {
+        char[] tobeReversed = sentence.ToCharArray();
+        Array.Reverse(tobeReversed);
+        return new string(tobeReversed);
+
+    }
+
+    private string FullName(string firstName, string lastName)
+    {
+        return $"{firstName}{lastName}";
+    }
+
+
+    public string Greet(string name, bool isReversed = false)
+    {
+        if (isReversed)
+        {
+            var reversedName = Reverser(name);
+
+            return $"Hello! My name is {reversedName}.";
+        }
+
+        return $"Hello! My name is {name}.";
+    }
+
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new Person();
+
+        Console.WriteLine(person.Greet("Anakin", true));
+    }
+}
