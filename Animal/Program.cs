a90fc02e80b98cd456a7a8e4ff5d209ede5de1c0
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-24 16:18:05 -0300

    Animal example

---
+++ Animal/Program.cs
@@ -0,0 +1,33 @@
+﻿interface IAnimal {
+  string MakeSound();
+}
+
+public class Cat : IAnimal
+{
+
+    public int Paws;
+    public string Name;
+
+    public Cat(int paws, string name)
+    {
+        Paws = paws;
+        Name = name;
+
+    }
+
+    public string MakeSound()
+    {
+        return "Miau!";
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var dante = new Cat(paws: 4, name: "Dante");
+        Console.WriteLine($"The cat is called {dante.Name}.");
+        Console.WriteLine($"The cat has {dante.Paws} paws.");
+
+    }
+}
