7e1407c505fa93852532c267fe65ac8f4f3ebb6e
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-25 11:03:09 -0300

    Adds a class BoardGame I've dreamed about tonight

---
+++ BoardGame/Program.cs
@@ -0,0 +1,24 @@
+﻿class BoardGame
+{
+    static int CardsInThePile { get; set; } = 200;
+
+    public BoardGame()
+    {
+        CardsInThePile -= 4;
+    }
+
+    public static string ShowCardsLeft()
+    {
+        return $"There are {CardsInThePile} cards left in the pile.";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var luiza = new BoardGame();
+            var chomsky = new BoardGame();
+            Console.WriteLine(BoardGame.ShowCardsLeft());
+        }
+    }
+}
